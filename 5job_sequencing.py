e705c7ee6e5457232cb45a9cfaf3e6c741ada30f
Nimisha Jha <nimishajha1103@gmail.com>
2022-10-18 23:55:23 +0530

    job_sequencing

---
+++ 5job_sequencing.py
@@ -0,0 +1,48 @@
+Job = (input("enter the job value: "))
+Job = Job.split()
+
+Job = [(x) for x in Job]
+
+Profit = (input("enter the profit value: "))
+Profit = Profit.split()
+
+Profit = [int(y) for y in Profit]
+
+deadline = (input("enter the deadline value: "))
+deadline = deadline.split()
+
+deadline = [int(z) for z in deadline]
+
+
+sorted_array = [(Job[i], Profit[i], deadline[i]) for i in range(0,len(Profit))]
+
+max_deadline = sorted(sorted_array, key=lambda i: i[2],reverse=True)
+n = max_deadline[0][2]
+
+arr = sorted(sorted_array, key=lambda i: i[2], reverse=True)
+
+def job_seq(max_deadline,arr):
+    d = []
+    for i in range(max_deadline):
+        max_item = arr[0]
+        for item in arr:
+            if item[2] == i+1: 
+                if item[1] > max_item[1]:
+                    max_item = item
+
+        d.append(max_item)
+    return d
+print(job_seq(n,arr))
+
+
+
+
+
+
+
+
+
+
+
+
+
